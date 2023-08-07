a61c38d8b140e8658e8c817b24a7a492c61cb8a9
User <alicsebaiust01@gmail.com>
2023-08-08 02:35:24 +0600

    Card

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -2,6 +2,7 @@
 //import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
 //import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
 //import 'package:ecommerce_shopanbd/ui/utills/styles.dart';
+//import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
 import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
 import 'package:flutter/material.dart';
 //import 'package:get/get.dart';
@@ -50,61 +51,123 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            const SearchTextField(),
-            const SizedBox(
-              height: 16,
-            ),
-            HomeCarousalWidget(),
-            const SizedBox(
-              height: 16,
-            ),
-            RemarksTitleWidget(
-              remarksName: 'Categories',
-              onTapSeeAll: (){},
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: const [
-                  CategoryCardWidget(
-                    name: 'Computer',
-                  ),
-                  CategoryCardWidget(
-                    name: 'Electronics',
-                  ),
-                  CategoryCardWidget(
-                    name: 'Clothes',
-                  ),
-                  CategoryCardWidget(
-                    name: 'Computer',
-                  ),
-                  CategoryCardWidget(
-                    name: 'Computer',
-                  ),
-                  CategoryCardWidget(
-                    name: 'Computer',
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SearchTextField(),
+              const SizedBox(
+                height: 16,
               ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            RemarksTitleWidget(
-              remarksName: 'Popular',
-              onTapSeeAll: (){},
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-          ],
+              HomeCarousalWidget(),
+              const SizedBox(
+                height: 16,
+              ),
+              RemarksTitleWidget(
+                remarksName: 'Categories',
+                onTapSeeAll: (){},
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: const [
+                    CategoryCardWidget(
+                      name: 'Computer',
+                    ),
+                    CategoryCardWidget(
+                      name: 'Electronics',
+                    ),
+                    CategoryCardWidget(
+                      name: 'Clothes',
+                    ),
+                    CategoryCardWidget(
+                      name: 'Computer',
+                    ),
+                    CategoryCardWidget(
+                      name: 'Computer',
+                    ),
+                    CategoryCardWidget(
+                      name: 'Computer',
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              RemarksTitleWidget(
+                remarksName: 'Popular',
+                onTapSeeAll: (){},
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              SizedBox(
+                width: 140,
+                child: Card(
+                  elevation: 4,
+                  child: Column(
+                    children: [
+                      Image.asset('assets/images/dummy_shoe.jpg',
+                        width: 132, height: 60,
+                      fit: BoxFit.cover,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                          Text('Nike Casual Shoe A3456',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 13,
+                            color: greyColor,
+                          ),),
+                        Row(
+                          children: [
+                          Text('\$340', style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 12,
+                          color: primaryColor,
+                          ),
+                          ),
+                          ],
+                        ),
+                      const SizedBox(
+                          width: 4, ,
+                      ),
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          const Icon(Icons.star, color: Colors.amber,
+                            size: 14,),
+                          Text('4.5')
+                        ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: primaryColor,
+                            borderRadius: BorderRadius.circular(4)
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.favorite_border),
+                        ),
+                      )
+                    ],
+                  ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+      )
+      ],
+    ),
+    ),
+    ),
     );
   }
 }
