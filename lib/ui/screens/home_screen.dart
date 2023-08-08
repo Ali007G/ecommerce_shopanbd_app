832ab05b4f7ece27185c32f05834d24f18a87560
User <alicsebaiust01@gmail.com>
2023-08-08 17:45:49 +0600

    home screen product

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -13,6 +13,7 @@ import '../widgets/home/app_bar_icon_button.dart';
 import '../widgets/home/home_carousal_widget.dart';
 import '../widgets/home/remarks_title_widget.dart';
 import '../widgets/home/search_text_field.dart';
+import '../widgets/product_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -117,6 +118,50 @@ class _HomeScreenState extends State<HomeScreen> {
                   ],
                 ),
           ),
+///
+              const SizedBox(
+                height: 16,
+              ),
+              RemarksTitleWidget(
+                remarksName: 'Special',
+                onTapSeeAll: () {},
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 16,
+              ),
+              RemarksTitleWidget(
+                remarksName: 'New',
+                onTapSeeAll: () {},
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                    ProductCard(),
+                  ],
+                ),
+              ),
           ],
         ),
       ),
@@ -125,92 +170,4 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class ProductCard extends StatelessWidget {
-  const ProductCard({
-    Key? key,
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 140,
-      child: Card(
-        elevation: 5,
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/dummy_shoe.jpg',
-              width: 132,
-              height: 80,
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text(
-                    'Nike Casual Shoe A3456',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 13,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        '\$340',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 12,
-                          letterSpacing: 0.3,
-                          height: 1.2,
-                          color: primaryColor,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 4,
-                      ),
-                      Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                            size: 16,
-                          ),
-                          Text('4.5',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 12,
-                              color: greyColor,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: primaryColor,
-                            borderRadius: BorderRadius.circular(4)),
-                        child: const Padding(
-                          padding: EdgeInsets.all(3.0),
-                          child: Icon(Icons.favorite_border, size: 12, color: Colors.white,),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/ui/widgets/product_card.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+import '../utills/app_colours.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 140,
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        shadowColor: primaryColor.withOpacity(0.2),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dummy_shoe.jpg',
+              width: 132,
+              height: 80,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Nike Casual Shoe A3456',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 13,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '\$340',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 12,
+                          letterSpacing: 0.3,
+                          height: 1.2,
+                          color: primaryColor,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 16,
+                          ),
+                          Text(
+                            '4.5',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 12,
+                              color: greyColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: primaryColor,
+                            borderRadius: BorderRadius.circular(4)),
+                        child: const Padding(
+                          padding: EdgeInsets.all(3.0),
+                          child: Icon(
+                            Icons.favorite_border,
+                            size: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
