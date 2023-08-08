e7ec0c073571a174ec83aea9d6bbee45d44dcd72
User <alicsebaiust01@gmail.com>
2023-08-08 22:45:26 +0600

    product list ui added

---
+++ lib/ui/screens/product_list_screen.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
+import 'package:ecommerce_shopanbd/ui/widgets/product_card.dart';
+import 'package:flutter/material.dart';
+
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product'),
+        leading: const BackButton(
+          color: greyColor,
+        ),
+      ),
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, childAspectRatio: 0.75
+          ),
+          itemCount: 50,
+          itemBuilder: (context, index) {
+            return const ProductCard();
+          }),
+    );
+  }
+}
--- lib/ui/widgets/catrgory_card_widget.dart
+++ lib/ui/widgets/catrgory_card_widget.dart
@@ -1,6 +1,8 @@
+import 'package:ecommerce_shopanbd/ui/screens/product_list_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../utills/app_colours.dart';
+import 'package:get/get.dart';
 
 class CategoryCardWidget extends StatelessWidget {
   const CategoryCardWidget({
@@ -12,30 +14,35 @@ class CategoryCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 6),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                color: primaryColor.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(8)
+    return InkWell(
+      onTap: (){
+        Get.to(const ProductListScreen());
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 6),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: primaryColor.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(8)
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Icon(Icons.computer, size: 30, color: primaryColor,),
+              ),
             ),
-            child: const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Icon(Icons.computer, size: 30, color: primaryColor,),
-            ),
-          ),
-          const SizedBox(height: 8),
-          Text(name, textAlign: TextAlign.center,
-            style: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: primaryColor,
-                letterSpacing: 0.6,
-                fontSize: 14
-            ),),
-        ],
+            const SizedBox(height: 8),
+            Text(name, textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  color: primaryColor,
+                  letterSpacing: 0.6,
+                  fontSize: 14
+              ),),
+          ],
+        ),
       ),
     );
   }
