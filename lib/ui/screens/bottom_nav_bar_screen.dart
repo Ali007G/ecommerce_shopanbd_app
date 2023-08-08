bd842f53b899d90023d01e051b3d33c493c8d5ba
User <alicsebaiust01@gmail.com>
2023-08-08 20:20:28 +0600

    bottom navigation, category, controller on process

--- lib/ui/screens/bottom_nav_bar_screen.dart
+++ lib/ui/screens/bottom_nav_bar_screen.dart
@@ -4,6 +4,9 @@ import 'package:ecommerce_shopanbd/ui/screens/home_screen.dart';
 import 'package:ecommerce_shopanbd/ui/screens/wish_list_screen.dart';
 import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../state_management/bottom_navigation_bar_controller.dart';
 
 class BottomNavBarScreen extends StatefulWidget {
   const BottomNavBarScreen({Key? key}) : super(key: key);
@@ -13,7 +16,8 @@ class BottomNavBarScreen extends StatefulWidget {
 }
 
 class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
-
+  BottomNavigationBarController _navigationBarController =
+  Get.put(BottomNavigationBarController());
   final List<Widget> _screens =[ const
     HomeScreen(),
     CategoryScreen(),
@@ -21,56 +25,58 @@ class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
     WishListScreen(),
   ];
 
-  int _selectedIndex = 0;
+  //int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (value){
-          //print(value);
-          _selectedIndex = value;
-          if(mounted){
-            setState(() {
-
-            });
-          }
-        },
-        elevation: 5,
-        selectedItemColor: primaryColor,
-        unselectedItemColor: greyColor,
-        currentIndex: _selectedIndex,
-        showUnselectedLabels: true,
-        unselectedLabelStyle: TextStyle(
-          color: greyColor,
-        ),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined,
+      body: GetBuilder<BottomNavigationBarController>(
+        builder: (controller) {
+          return _screens[controller.selectedIndex];
+        }
+      ),
+      bottomNavigationBar: GetBuilder<BottomNavigationBarController>(
+        builder: (controller) {
+          return BottomNavigationBar(
+            onTap: (value){
+              controller.changeIndex(value);
+            },
+            elevation: 5,
+            selectedItemColor: primaryColor,
+            unselectedItemColor: greyColor,
+            currentIndex: controller.selectedIndex,
+            showUnselectedLabels: true,
+            unselectedLabelStyle: TextStyle(
+              color: greyColor,
             ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.grid_view,
-            ),
-            label: 'Categories',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.shopping_cart_outlined,
-            ),
-            label: 'Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.favorite_border,
-            ),
-            label: 'Wishlist'
-          ),
-        ],
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                ),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.grid_view,
+                ),
+                label: 'Categories',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.shopping_cart_outlined,
+                ),
+                label: 'Cart',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite_border,
+                ),
+                label: 'Wishlist'
+              ),
+            ],
+          );
+        }
       ),
     );
   }
