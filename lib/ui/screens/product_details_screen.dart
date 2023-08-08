b52954bd14ee1181f40abd4bb38db4183c3e331d
User <alicsebaiust01@gmail.com>
2023-08-09 02:40:27 +0600

    product details ui in progress

--- lib/ui/screens/product_details_screen.dart
+++ lib/ui/screens/product_details_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
 import 'package:ecommerce_shopanbd/ui/utills/styles.dart';
 import 'package:ecommerce_shopanbd/ui/widgets/product_details/product_image_carousal.dart';
+import 'package:ecommerce_shopanbd/ui/widgets/product_stepper.dart';
 import 'package:flutter/material.dart';
 import 'package:numberpicker/numberpicker.dart';
 
@@ -12,7 +15,6 @@ class ProductDetailsScreen extends StatefulWidget {
 }
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
-  int _currentValue = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,119 +26,96 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
         ),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ProductImageCarousal(),
-          Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Text('Adidas Casual Shoe s463 Brand New',
-                    style: titleTextStyle.copyWith(
-                      fontSize: 18,
-                    )),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Wrap(
-                          crossAxisAlignment: WrapCrossAlignment.center,
-                          children: [
-                            const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                              size: 15,
-                            ),
-                            Text(
-                              '4.5',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 12,
-                                color: greyColor,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Adidas Casual Shoe s463 Brand New',
+                          style: titleTextStyle.copyWith(
+                            fontSize: 18,
+                          )),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Wrap(
+                                crossAxisAlignment: WrapCrossAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                    size: 15,
+                                  ),
+                                  Text(
+                                    '4.5',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 12,
+                                      color: greyColor,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        TextButton(
-                          onPressed: (){},
-                          child: const Text(
-                            'Reviews',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 15,
-                              letterSpacing: 0.3,
-                              height: 1.2,
-                              color: primaryColor,
-                            ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              TextButton(
+                                onPressed: (){},
+                                child: const Text(
+                                  'Reviews',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 15,
+                                    letterSpacing: 0.3,
+                                    height: 1.2,
+                                    color: primaryColor,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 4,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: primaryColor,
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(4.0),
+                                  child: Icon(
+                                    Icons.favorite_border,
+                                    size: 12,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: primaryColor,
-                              borderRadius: BorderRadius.circular(5)),
-                          child: const Padding(
-                            padding: EdgeInsets.all(4.0),
-                            child: Icon(
-                              Icons.favorite_border,
-                              size: 12,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const ProductStepper()
                   ],
                 ),
-              ),
-              Column(
-                children: [
-                  NumberPicker(
-                    value: _currentValue,
-                    minValue: 0,
-                    maxValue: 100,
-                    step: 1,
-                    itemHeight: 40,
-                    itemWidth: 30,
-                    axis: Axis.horizontal,
-                    onChanged: (value) =>
-                        setState(() => _currentValue = value),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(color: Colors.black26),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.remove),
-                        onPressed: () => setState(() {
-                          final newValue = _currentValue - 1;
-                          _currentValue = newValue.clamp(0, 100);
-                        }),
-                      ),
-                     // Text('Current horizontal int value: $_currentValue'),
-                      IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () => setState(() {
-                          final newValue = _currentValue + 1;
-                          _currentValue = newValue.clamp(0, 100);
-                        }),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          )
+                const Text('Color'),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+
+
