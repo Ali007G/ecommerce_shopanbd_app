8b39ea1bb5f8278efa185fc4749fcb2cab367c62
User <alicsebaiust01@gmail.com>
2023-08-10 22:29:08 +0600

    product details ui done

--- lib/ui/screens/product_details_screen.dart
+++ lib/ui/screens/product_details_screen.dart
@@ -1,6 +1,6 @@
-
 import 'package:ecommerce_shopanbd/ui/utills/app_colours.dart';
 import 'package:ecommerce_shopanbd/ui/utills/styles.dart';
+import 'package:ecommerce_shopanbd/ui/widgets/common_elevated_button.dart';
 import 'package:ecommerce_shopanbd/ui/widgets/product_details/product_image_carousal.dart';
 import 'package:ecommerce_shopanbd/ui/widgets/product_stepper.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +13,6 @@ class ProductDetailsScreen extends StatefulWidget {
 }
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
-
   List<Color> colors = const [
     Color(0xFF000000),
     Color(0xFFB31818),
@@ -21,15 +20,9 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
     Color(0xFF53B318),
     Color(0xFF34C4C4),
   ];
-  List<String> sizes = const [
-    'S',
-    'L',
-    'XL',
-    'XXl',
-    'XXXL'
-  ];
+  List<String> sizes = const ['S', 'L', 'XL', 'XXl', 'XXXL'];
 
-  Color _selectedCColor = Color(0xFF000000);
+  Color _selectedColor = const Color(0xFF000000);
   String _selectedSize = 'L';
 
   @override
@@ -44,160 +37,263 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ProductImageCarousal(),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Adidas Casual Shoe s463 Brand New',
-                          style: titleTextStyle.copyWith(
-                            fontSize: 18,
-                          )),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ProductImageCarousal(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  const Icon(
-                                    Icons.star,
-                                    color: Colors.amber,
-                                    size: 15,
-                                  ),
-                                  Text(
-                                    '4.5',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 12,
-                                      color: greyColor,
-                                    ),
+                                  Text('Adidas Casual Shoe s463 Brand New',
+                                      style: titleTextStyle.copyWith(
+                                        fontSize: 18,
+                                      )),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Wrap(
+                                        crossAxisAlignment:
+                                            WrapCrossAlignment.center,
+                                        children: const [
+                                          Icon(
+                                            Icons.star,
+                                            color: Colors.amber,
+                                            size: 15,
+                                          ),
+                                          Text(
+                                            '4.5',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 12,
+                                              color: greyColor,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(
+                                        width: 8,
+                                      ),
+                                      TextButton(
+                                        onPressed: () {},
+                                        child: const Text(
+                                          'Reviews',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 15,
+                                            letterSpacing: 0.3,
+                                            height: 1.2,
+                                            color: primaryColor,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            color: primaryColor,
+                                            borderRadius:
+                                                BorderRadius.circular(5)),
+                                        child: const Padding(
+                                          padding: EdgeInsets.all(4.0),
+                                          child: Icon(
+                                            Icons.favorite_border,
+                                            size: 12,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
-                              const SizedBox(
-                                width: 8,
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            const ProductStepper(),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const Text(
+                          'Color',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: greyColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Row(
+                            children: colors.map((color) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(12),
+                              onTap: () {
+                                if (_selectedColor != color) {
+                                  _selectedColor = color;
+                                  if (mounted) {
+                                    setState(() {});
+                                  }
+                                }
+                              },
+                              child: CircleAvatar(
+                                backgroundColor: color,
+                                radius: 15,
+                                child: Visibility(
+                                    visible: color == _selectedColor,
+                                    child: const Icon(
+                                      Icons.check,
+                                      color: Colors.white,
+                                    )),
                               ),
-                              TextButton(
-                                onPressed: (){},
-                                child: const Text(
-                                  'Reviews',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 15,
-                                    letterSpacing: 0.3,
-                                    height: 1.2,
-                                    color: primaryColor,
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 4,
-                              ),
-                              Container(
+                            ),
+                          );
+                        }).toList()),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const Text(
+                          'Size',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: greyColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Row(
+                            children: sizes.map((size) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(12),
+                              onTap: () {
+                                if (_selectedSize != size) {
+                                  _selectedSize = size;
+                                  if (mounted) {
+                                    setState(() {});
+                                  }
+                                }
+                              },
+                              child: Container(
                                 decoration: BoxDecoration(
-                                    color: primaryColor,
-                                    borderRadius: BorderRadius.circular(5)),
-                                child: const Padding(
-                                  padding: EdgeInsets.all(4.0),
-                                  child: Icon(
-                                    Icons.favorite_border,
-                                    size: 12,
-                                    color: Colors.white,
+                                  borderRadius: BorderRadius.circular(6),
+                                  border: Border.all(
+                                    color: Colors.grey,
+                                  ),
+                                  color: size == _selectedSize
+                                      ? primaryColor
+                                      : null,
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    size,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        color: size == _selectedSize
+                                            ? Colors.white
+                                            : null),
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    const ProductStepper(),
-                    ],
-                ),
-                const SizedBox(height: 16,),
-                const Text('Color', style: TextStyle(
-                  fontSize: 20,
-                  color: greyColor,
-                  fontWeight: FontWeight.w600,
-                ),),
-                    const SizedBox(height: 16,),
-                    Row(
-                        children: colors.map((color) {
-                      return Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(12),
-                          onTap: () {
-                            if(_selectedCColor != color) {
-                              _selectedCColor = color;
-                              if (mounted) {
-                                setState(() {
-
-                                });
-                              }
-                            }
-                          },
-                          child: CircleAvatar(
-                            backgroundColor: color,
-                            radius: 15,
-                            child: Visibility(
-                                visible: color == _selectedCColor,
-                                child: Icon(Icons.check, color: Colors.white,)),
+                            ),
+                          );
+                        }).toList()),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const Text(
+                          'Description',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: greyColor,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      );
-                    }).toList()),
-
-                const SizedBox(height: 16,),
-                const Text('Size', style: TextStyle(
-                  fontSize: 20,
-                  color: greyColor,
-                  fontWeight: FontWeight.w600,
-                ),),
-                const SizedBox(height: 16,),
-                Row(
-                    children: sizes.map((size) {
-                      return Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(12),
-                          onTap: () {
-                            if(_selectedSize != size) {
-                              _selectedSize = size;
-                              if (mounted) {
-                                setState(() {
-
-                                });
-                              }
-                            }
-                          },
-                          child: CircleAvatar(
-                            radius: 15,
-                            child: Visibility(
-                                visible: size == _selectedSize,
-                                child: Icon(Icons.check, color: Colors.white,)),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const Text(
+                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: softGreyColor,
+                            //fontWeight: FontWeight.w400,
                           ),
                         ),
-                      );
-                    }).toList())
-              ],
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
+          Container(
+            decoration: BoxDecoration(
+              color: primaryColor.withOpacity(0.3),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+              ),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Price',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: softGreyColor,
+                        ),
+                      ),
+                      Text(
+                        '\$100',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 120,
+                    child: CommonElevatedButton(
+                      onTap: () {},
+                      title: 'Add to Cart',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 }
-
-
-
--- lib/ui/utills/app_colours.dart
+++ lib/ui/utills/app_colours.dart
@@ -3,5 +3,5 @@ import 'dart:ui';
 const Color primaryColor = Color(0xFF07ADAE);
 const Color greyColor = Color(0xFFAAB5BB);
 const Color blackColor = Color(0xFF000000);
-const Color softGreyColor = Color(0xAAB5BBFF);
+const Color softGreyColor = Color(0x707174AA);
 
