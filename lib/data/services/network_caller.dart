1c05bec5a28198e448087d68d1333c6b48445122
User <alicsebaiust01@gmail.com>
2023-08-11 15:52:15 +0600

    network caller code added

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -1,11 +1,38 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:math';
+
+import 'package:ecommerce_shopanbd/data/models/response_model.dart';
+import 'package:ecommerce_shopanbd/data/utils/urls.dart';
 import 'package:http/http.dart';
 
 class NetworkCaller {
   //singleton
   NetworkCaller._();
 
-  static Future<dynamic> getRequest() async {
-
-
+  static Future<ResponseModel> getRequest({required String url}) async {
+    try {
+    final Response response = await get(Uri.parse(Urls.baseUrl + url));
+      if (response.statusCode == 200) {
+        return ResponseModel(
+          isSuccess: true,
+            statusCode: response.statusCode,
+            returnData: jsonDecode(response.body)
+        );
+      } else {
+        return ResponseModel(
+        isSuccess: false,
+        statusCode: response.statusCode,
+      returnData: jsonDecode(response.body)
+        );
+      }
+    } catch (e) {
+      log(e.toString());
+      return ResponseModel(
+          isSuccess: false,
+          statusCode: -1,
+          returnData: e.toString()
+      );
+    }
   }
 }
